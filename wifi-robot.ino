537fa24b19f6bfc0cef265b7f1cd18f609abd46e
aawzard <iec2019097@iiita.ac.in>
2021-04-09 21:22:59 +0530

    first commit

---
+++ wifi-robot.ino
@@ -0,0 +1,121 @@
+
+#define BLYNK_PRINT Serial
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+
+static const uint8_t D5=14;
+static const uint8_t D6=12;
+static const uint8_t D7=13;
+static const uint8_t D8=15;
+
+#define RightMotorSpeed       D5   //14 
+#define RightMotorDir         D6   //12 
+#define LeftMotorSpeed        D7   //13 
+#define LeftMotorDir          D8   //15 
+
+char auth[] = "----------------";  //Blynk Authentication Token
+char ssid[] = "-------";           //WIFI Name
+char pass[] = "-------";           //WIFI Password
+
+int minRange = 312;
+int maxRange = 712;
+
+int minspeed = 450;
+int maxspeed = 1020;
+int nospeed = 0;
+
+
+void moveControl(int x, int y)
+{
+//Move Forward
+    if(y >= maxRange && x >= minRange && x<= maxRange)
+    {
+        digitalWrite( RightMotorDir,HIGH);
+        digitalWrite(LeftMotorDir,HIGH);
+        analogWrite(RightMotorSpeed, maxspeed);
+        analogWrite(LeftMotorSpeed , maxspeed);
+    }
+
+
+//Move Forward Right
+    else if(x >= maxRange && y >= maxRange)
+    {
+        digitalWrite( RightMotorDir, HIGH);
+        digitalWrite(LeftMotorDir,HIGH);
+        analogWrite(RightMotorSpeed,minspeed);
+        analogWrite(LeftMotorSpeed ,maxspeed);
+    }
+
+//Move Forward Left
+    else if(x <= minRange && y >= maxRange)
+    {
+        digitalWrite( RightMotorDir,HIGH);
+        digitalWrite(LeftMotorDir,HIGH);
+        analogWrite(RightMotorSpeed,maxspeed);
+        analogWrite(LeftMotorSpeed ,minspeed);
+    }
+
+//No Move
+    else if(y < maxRange && y > minRange && x < maxRange && x > minRange)
+    {
+        analogWrite(RightMotorSpeed,nospeed);
+        analogWrite(LeftMotorSpeed , nospeed);
+    }
+
+//Move Backward
+    else if(y <= minRange && x >= minRange && x <= maxRange)
+    {
+        digitalWrite( RightMotorDir,LOW);
+        digitalWrite(LeftMotorDir,LOW);
+        analogWrite(RightMotorSpeed,maxspeed);
+        analogWrite(LeftMotorSpeed ,maxspeed);
+    }
+
+//Move Backward Right
+    else if(y <= minRange && x <= minRange)
+    {
+        digitalWrite( RightMotorDir,LOW);
+        digitalWrite(LeftMotorDir,LOW);
+        analogWrite(RightMotorSpeed,minspeed);
+        analogWrite(LeftMotorSpeed ,maxspeed);
+    }
+
+//Move Backward Left
+    else if(y <= minRange && x >= maxRange)
+    {
+        digitalWrite( RightMotorDir,LOW);
+        digitalWrite(LeftMotorDir,LOW);
+        analogWrite(RightMotorSpeed,maxspeed);
+        analogWrite(LeftMotorSpeed ,minspeed);
+    }
+}
+
+void setup()
+{
+    Serial.begin(9600);
+    Blynk.begin(auth, ssid, pass);
+
+    pinMode(RightMotorSpeed, OUTPUT);
+    pinMode(LeftMotorSpeed , OUTPUT);
+    pinMode( RightMotorDir, OUTPUT);
+    pinMode(LeftMotorDir, OUTPUT);
+
+    digitalWrite(RightMotorSpeed, LOW);
+    digitalWrite(LeftMotorSpeed , LOW);
+    digitalWrite( RightMotorDir, HIGH);
+    digitalWrite(LeftMotorDir, HIGH);
+}
+
+
+void loop()
+{
+    Blynk.run();
+}
+
+
+BLYNK_WRITE(V1)
+{
+    int x = param[0].asInt();
+    int y = param[1].asInt();
+    moveControl(x,y);
+}
